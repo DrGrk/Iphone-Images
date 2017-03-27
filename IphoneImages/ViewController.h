c47362485a913a07760e76363c5916abd9f7fec9
David Guichon <davidrguichon@gmail.com>
2017-03-27 16:24:35 -0400

    Initial Commit

---
+++ IphoneImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IphoneImages
+//
+//  Created by David Guichon on 2017-03-27.
+//  Copyright © 2017 David Guichon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IphoneImages/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IphoneImages
+//
+//  Created by David Guichon on 2017-03-27.
+//  Copyright © 2017 David Guichon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
